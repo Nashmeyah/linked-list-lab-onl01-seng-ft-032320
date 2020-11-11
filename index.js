35987f9bdb94281e5665b4061339fdc7c77dd9a5
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-11-11 05:32:15 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+let firstNode = {name: 'susie', next: 'rkjasj'}
+let secondNode = {name: 'sam', next: 'asnan'}
+let lastNode = {name: 'charlie', next: null}
+let collection = {rkjasj: secondNode, asnan: lastNode, whana: firstNode}
+let linkedList = 'whana'
+let head = collection[linkedList]
